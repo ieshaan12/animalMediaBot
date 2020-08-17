533a600ec4e395463e29e998b0b9bf58e35541c0
ieshaan12 <ieshaan1999@gmail.com>
2020-08-17 23:11:40 +0530

    Made some edits

--- animalMediaBot.py
+++ animalMediaBot.py
@@ -1,5 +1,6 @@
 import praw
 import csv
+import os
 
 class animalMediaBot:
     def __init__(self):
@@ -7,9 +8,10 @@ class animalMediaBot:
         self.daily_message_beg = "Hello your daily quota of posts is here as follows\n\n\n"
         self.daily_message_end = "Sent with love from /u/animalMediaBot by /u/ieshaan12"
         self.subject = "Your daily digest from r/"      
+        self.subDoesNotExist = "This subreddit doesn't exist/ it is private - "
     
-    def getCredentials(self):
-        f = open('credentials.txt')
+    def getCredentials(self, credentialFile):
+        f = open(credentialFile)
         self.client_id, self.client_secret, self.username, self.password, self.user_agent = f.readline().split(',')
         f.close()
 
@@ -20,11 +22,11 @@ class animalMediaBot:
                     password = self.password, 
                     user_agent = self.user_agent)
     
-    def getUserData(self):
+    def getUserData(self, userFile):
         self.subredditDict = dict()
         self.allSubs = set()
 
-        with open('userCSV.csv') as csvFile:
+        with open(userFile) as csvFile:
             csvData = csv.reader(csvFile,delimiter=',')
             for row in csvData:
                 username = row.pop(0)
@@ -39,17 +41,20 @@ class animalMediaBot:
             subreddit = self.reddit.subreddit(i)
             counter = 1
             Message = self.daily_message_beg
-            for submission in subreddit.hot(limit=12):
-                if not submission.stickied:
-                    newLine = str(counter) + ': ' + submission.permalink + '\n\n'
-                    Message += newLine
-                    counter += 1
-                if counter > 10:
-                    break
-            Message += '\n' + self.daily_message_end
+            try:
+                for submission in subreddit.hot(limit=12):
+                    if not submission.stickied:
+                        newLine = str(counter) + ': ' + submission.permalink + '\n\n'
+                        Message += newLine
+                        counter += 1
+                    if counter > 10:
+                        break
+                Message += '\n' + self.daily_message_end
 
-            self.subredditMessageData[i] = Message
-    
+                self.subredditMessageData[i] = Message
+            except:
+                self.subredditMessageData[i] = self.subDoesNotExist + i
+        
     def sendMessageData(self):
         
         for username,subreddits in self.subredditDict.items():
@@ -57,9 +62,14 @@ class animalMediaBot:
                 self.reddit.redditor(username).message(self.subject + subreddit, self.subredditMessageData[subreddit])
 
 if __name__ == "__main__":
+    # Change your working directory here for using your own credentials and user subreddit file 
+    WorkingDirectory = "C:/Users/Work/Desktop/VSCode/AnimalMediaBot/"
+    os.chdir(WorkingDirectory)
+    credentialFile = 'credentials.txt'
+    userFile = 'userCSV.csv'
     BOT = animalMediaBot()
-    BOT.getCredentials()
-    BOT.getUserData()
+    BOT.getCredentials(credentialFile)
+    BOT.getUserData(userFile)
     BOT.login()
     BOT.getAllMessageData()
     BOT.sendMessageData()
