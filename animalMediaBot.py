619375e4d30955ed7dee028ca802aeaf01eb6681
ieshaan12 <ieshaan1999@gmail.com>
2020-08-18 23:20:35 +0530

    Added functionality to parse inbox and add users

--- animalMediaBot.py
+++ animalMediaBot.py
@@ -1,6 +1,8 @@
 import praw
 import csv
 import os
+import re
+import pprint
 
 class animalMediaBot:
     def __init__(self):
@@ -9,7 +11,17 @@ class animalMediaBot:
         self.daily_message_end = "Sent with love from /u/animalMediaBot by /u/ieshaan12"
         self.subject = "Your daily digest from r/"      
         self.subDoesNotExist = "This subreddit doesn't exist/ it is private - "
-    
+
+    def getFiles(self, fileData = 'fileData.txt'):
+        with open(fileData) as csvFile:
+            csvData = csv.reader(csvFile,delimiter=',')
+            for row in csvData:
+                workingDirectory = row[0]
+                credentialFile = row[1]
+                userFile = row[2]
+                newDataFile = row[3]
+        return workingDirectory, credentialFile, userFile, newDataFile
+
     def getCredentials(self, credentialFile):
         f = open(credentialFile)
         self.client_id, self.client_secret, self.username, self.password, self.user_agent = f.readline().split(',')
@@ -29,10 +41,16 @@ class animalMediaBot:
         with open(userFile) as csvFile:
             csvData = csv.reader(csvFile,delimiter=',')
             for row in csvData:
-                username = row.pop(0)
-                subreddits = set(row)
-                self.subredditDict[username] = subreddits
-                self.allSubs.update(subreddits)
+                try:
+                    username = row.pop(0)
+                    subreddits = set(row)
+                    if username not in self.subredditDict:
+                        self.subredditDict[username] = subreddits
+                    else:
+                        self.subredditDict[username].update(subreddits)
+                    self.allSubs.update(subreddits)
+                except:
+                    print("Empty row - ", row)
 
     def getAllMessageData(self):
         self.subredditMessageData = dict()
@@ -61,16 +79,45 @@ class animalMediaBot:
             for subreddit in subreddits:
                 self.reddit.redditor(username).message(self.subject + subreddit, self.subredditMessageData[subreddit])
 
+    def getInbox(self, newDataFile = 'newUsers.csv'):
+        newUserData = dict()
+        for item in self.reddit.inbox.unread(limit = None):
+            subreddits = re.split(r'[,\s]\s*', item.body)
+            subs = []
+            for i in subreddits:
+                try:
+                    subs.append(i[2:])
+                except:
+                    pass
+            newUserData[item.author.name] = subs
+        os.remove(newDataFile)
+        with open(newDataFile,'w',newline='') as csvFile:
+            csvWriter = csv.writer(csvFile)
+            rows = []
+            for username, subreddits in newUserData.items():
+                subreddits.insert(0,username)
+                rows.append(subreddits)
+            csvWriter.writerows(rows)
+    
+    def mergeCSV(self, newUserFile = 'newUsers.csv', userFile = 'userCSV.csv'):
+        with open(userFile,'a') as csvWriteFile:
+            content = None
+            with open(newUserFile,'r') as csvReadFile:
+                content = csvReadFile.read()
+            csvWriteFile.write('\n')
+            csvWriteFile.write(content)
+
+
 if __name__ == "__main__":
-    # Change your working directory here for using your own credentials and user subreddit file 
-    WorkingDirectory = "C:/Users/Work/Desktop/VSCode/AnimalMediaBot/"
-    os.chdir(WorkingDirectory)
-    credentialFile = 'credentials.txt'
-    userFile = 'userCSV.csv'
     BOT = animalMediaBot()
+    workingDirectory, credentialFile, userFile, newDataFile = BOT.getFiles()
+    os.chdir(workingDirectory)
     BOT.getCredentials(credentialFile)
     BOT.getUserData(userFile)
     BOT.login()
     BOT.getAllMessageData()
     BOT.sendMessageData()
-        
+    
+    # For updating new users from inbox, call these functions
+    #BOT.getInbox()
+    #BOT.mergeCSV()
